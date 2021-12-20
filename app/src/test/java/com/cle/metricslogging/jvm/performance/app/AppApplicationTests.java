d34449583fed0bbb7f027fd02926f242471e9bda
Ann McDonald <mc_donald_ann@hotmail.co.uk>
2021-12-20 17:17:42 +0000

    initial commit

---
+++ app/src/test/java/com/cle/metricslogging/jvm/performance/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cle.metricslogging.jvm.performance.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
